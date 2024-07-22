c60be17fc2f60c158e902d35dc8bea23b357a77c
lychee xing <lolychee@gmail.com>
2024-07-22 08:16:41 +0000

    chore: init gem

---
+++ lib/flowkit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "flowkit/version"
+
+module Flowkit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/flowkit/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Flowkit
+  VERSION = "0.1.0"
+end
